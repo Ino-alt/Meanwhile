3e003a63283d3d400d0bca83b1389b1884ce7011
Ino-alt <reo45@icloud.com>
2026-04-08 13:32:57 +0900

    Add countries table and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+countries = [
+  { name: "韓国",       country_code: "KR", timezone: "Asia/Seoul" },
+  { name: "トルコ",     country_code: "TR", timezone: "Europe/Istanbul" },
+  { name: "ブラジル",   country_code: "BR", timezone: "America/Sao_Paulo" },
+  { name: "スウェーデン", country_code: "SE", timezone: "Europe/Stockholm" },
+  { name: "オーストラリア", country_code: "AU", timezone: "Australia/Sydney" },
+  { name: "ケニア",     country_code: "KE", timezone: "Africa/Nairobi" }
+]
+
+countries.each do |attrs|
+  Country.find_or_create_by!(country_code: attrs[:country_code]) do |country|
+    country.name     = attrs[:name]
+    country.timezone = attrs[:timezone]
+  end
+end
