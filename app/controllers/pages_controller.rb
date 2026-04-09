17bb4ca78facc958b3222b5d4382db363d2b6747
Ino-alt <reo45@icloud.com>
2026-04-09 14:00:28 +0900

    ランダム国選択・現地時刻表示機能を追加

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @moment_country = Country.order("RANDOM()").first
+    @local_time     = Time.now.in_time_zone(@moment_country.timezone)
   end
 end
