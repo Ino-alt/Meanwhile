b690018e58013c3d3c415a749a7d81b0b203fb3e
Ino-alt <reo45@icloud.com>
2026-04-13 18:51:32 +0900

    fix: country_codeにユニークインデックスを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_08_055232) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_13_094814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_08_055232) do
     t.string "name"
     t.string "timezone"
     t.datetime "updated_at", null: false
+    t.index ["country_code"], name: "index_countries_on_country_code", unique: true
   end
 
   create_table "entries", force: :cascade do |t|
